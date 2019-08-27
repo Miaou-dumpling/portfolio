1b250ac0ee9ff17b6ff27d6dd6088339bd1e4bd9
David Hoeffel <duckblade0@gmail.com>
2019-08-27 14:17:32 +0200

    ajout du modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,6 +33,25 @@
 		// Fix: Placeholder polyfill.
 			$('form').placeholder();
 
+        // Get the modal
+        		var modal = document.getElementById("myModal");
+
+		// Get the button that opens the modal
+				var btn = document.getElementById("rpg");
+
+
+		// When the user clicks on the button, open the modal
+				btn.onclick = function() {
+					modal.style.display = "block";
+				}
+
+		// When the user clicks anywhere outside of the modal, close it
+				window.onclick = function(event) {
+					if (event.target == modal) {
+						modal.style.display = "none";
+					}
+				}
+
 		// Prioritize "important" elements on mobile.
 			skel.on('+mobile -mobile', function() {
 				$.prioritize(
